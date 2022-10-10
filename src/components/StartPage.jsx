5d5107ffbcbfedd9c6fcced51de32b04bb662bf4
tovehydmark <tovehydmark@gmail.com>
2022-10-10 16:34:51 +0200

    responsive design

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -5,7 +5,14 @@ export function StartPage() {
   return (
     <>
       <h2>I am Tove, a front-end developer</h2>
-      <p></p>
+      <p>
+        Graduating from the Media institute in June 2022, I am a frontend
+        developer in the making who thrives at the border between the frontend
+        and the backend. I can build a variety of applications, from restaurant
+        table booking systems to games with chatt functionality, diarys, og in
+        pages and more. I am foremost a React developer with Node for the
+        backend, but I also use Angular. Currently, I'm learning NextJS.
+      </p>
     </>
   );
 }
