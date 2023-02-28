ac0245699c8bb1db97070c39c78765d7c85a615e
tovehydmark <tovehydmark@gmail.com>
2023-02-28 23:43:09 +0100

    some styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,37 +7,25 @@ export function Navigation() {
     <>
       <ul>
         <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? 'link-active' : 'link')}
-          >
-            Home
+          <NavLink to="/" className={({ isActive }) => (isActive ? 'link-active' : 'link')}>
+            home
           </NavLink>
         </li>
 
         <li>
-          <NavLink
-            to="/portfolio"
-            className={({ isActive }) => (isActive ? 'link-active' : 'link')}
-          >
-            Portfolio
+          <NavLink to="/portfolio" className={({ isActive }) => (isActive ? 'link-active' : 'link')}>
+            portfolio
           </NavLink>
         </li>
 
         <li>
-          <NavLink
-            to="/cv"
-            className={({ isActive }) => (isActive ? 'link-active' : 'link')}
-          >
-            CV
+          <NavLink to="/cv" className={({ isActive }) => (isActive ? 'link-active' : 'link')}>
+            cv
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/contact"
-            className={({ isActive }) => (isActive ? 'link-active' : 'link')}
-          >
-            Contact
+          <NavLink to="/contact" className={({ isActive }) => (isActive ? 'link-active' : 'link')}>
+            contact
           </NavLink>
         </li>
       </ul>
--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -4,14 +4,21 @@ import '../styles/style.scss';
 export function StartPage() {
   return (
     <>
-      <h1>tove hydmark</h1>
+      <div className="nameSection">
+        <h1>
+          <span className="tove">tove</span>
+          <span className="hydmark"> hydmark</span>
+        </h1>
+      </div>
+
       <article className="about">
         <p>
-          graduating from the media institute in june 2022, i am a
-          <span>frontend developer</span> in the making who thrives at the
-          border between frontend and backend. i build a variety of
-          applications, from booking systems to games with chat functionality,
-          diarys, log in pages and more. current interest: Next.js
+          graduating from the media institute in june 2022,{' '}
+          <strong>
+            i am a<span> frontend developer</span>{' '}
+          </strong>
+          in the making who thrives at the border between frontend and backend. i build a variety of applications, from
+          booking systems to games with chat functionality, diarys, log in pages and more. current interest: Next.js
         </p>
       </article>
     </>
