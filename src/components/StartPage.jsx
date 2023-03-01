ff6c2e60ce555b4fad666237ef2714c6aef563b5
tovehydmark <tovehydmark@gmail.com>
2023-03-01 20:33:49 +0100

    render competencies and animation

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import 'animate.css';
 import '../styles/style.scss';
 
 export function StartPage() {
+  const competenceList = [
+    'JavaScript',
+    'TypeScript',
+    'Next.js',
+    'Node.js',
+    'cypress.js',
+    'CSS',
+    'SASS',
+    'Mantine',
+    'Semantic UI',
+  ];
+
+  const printCompetences = competenceList.map((competence, i) => {
+    return (
+      <>
+        <p className="competences animate__animated animate__rollIn animate__delay-1s">{competence}</p>
+      </>
+    );
+  });
   return (
     <>
       <div className="nameSection">
@@ -21,6 +41,7 @@ export function StartPage() {
           booking systems to games with chat functionality, diarys, log in pages and more. current interest: Next.js
         </p>
       </article>
+      <div className="competences-section"> {printCompetences}</div>
     </>
   );
 }
