fbc486a6d864d72a1270060e73876292d67a5c75
tovehydmark <tovehydmark@gmail.com>
2023-03-15 19:50:48 +0100

    remove old colde, add new styling

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -3,48 +3,9 @@ import 'animate.css';
 import '../styles/style.scss';
 
 export function StartPage() {
-  const competenceList = [
-    'JavaScript',
-    'TypeScript',
-    'Next.js',
-    'Node.js',
-    'cypress.js',
-    'CSS',
-    'SASS',
-    'Mantine',
-    'Semantic UI',
-    'Figma',
-  ];
-
-  const printCompetences = competenceList.map((competence, i) => {
-    return (
-      <>
-        <p className="competences animate__animated animate__rollIn animate__delay-0.2">{competence}</p>
-      </>
-    );
-  });
   return (
     <>
-      <div className="nameSection">
-        <h1>
-          <span className="tove">tove</span>
-          <span className="hydmark"> hydmark</span>
-        </h1>
-      </div>
-      <div className="competences-wrapper">
-        <article className="about">
-          <p>
-            graduating from the media institute in june 2022,{' '}
-            <strong>
-              i am a<span> frontend developer</span>{' '}
-            </strong>
-            in the making who thrives at the border between frontend and backend. i build a variety of applications,
-            from booking systems to games with chat functionality, diarys, log in pages and more. current interest:
-            Next.js
-          </p>
-        </article>
-        <div className="competences-section"> {printCompetences}</div>
-      </div>
+      <div className="about">Start page</div>
     </>
   );
 }
