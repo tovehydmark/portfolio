1510b376d746493164b09f1f688950a95537ac31
tovehydmark <tovehydmark@gmail.com>
2023-03-16 23:12:38 +0100

    contact details added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/style.scss';
 
 export function Contact() {
+  const socialMedia = [
+    { media: 'Github', link: 'https://github.com/tovehydmark' },
+    { media: 'LinkedIn', link: 'https://www.linkedin.com/in/tove-hydmark/' },
+  ];
+
+  const contact = [
+    { media: 'Email', data: 'tovehydmark@gmailcom' },
+    { media: 'Phone', data: '+447476320569' },
+  ];
+  const printSocialMedia = socialMedia.map((media) => {
+    return (
+      <div className="social-media-icon">
+        <a href={media.link} target="_blank" rel="noreferrer">
+          {media.media}
+        </a>
+      </div>
+    );
+  });
+
+  const printContact = contact.map((item) => {
+    return (
+      <div className="social-media-icon">
+        <p>
+          {item.media}: {item.data}
+        </p>
+      </div>
+    );
+  });
   return (
     <>
-      <h1>Contact</h1>
+      <div className="social-media">
+        {printSocialMedia}
+        {printContact}
+      </div>
+      <div className="contact">
+        <h1>Contact</h1>
+        <p>
+          I'm currently living in <span className="enhanced">London</span> but I also have a home in{' '}
+          <span className="enhanced">Stockholm.</span> <br />
+          Send me an email or give me a call if you're interested in my work
+        </p>
+      </div>
     </>
   );
 }
