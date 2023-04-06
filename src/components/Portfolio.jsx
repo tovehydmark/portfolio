bc584ce87530e06b1436474cc2c395423d3a6fda
tovehydmark <tovehydmark@gmail.com>
2023-04-06 12:59:57 +0200

    styles

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import '../styles/style.scss';
+import { useState } from 'react';
 
 export function Portfolio() {
+  const [isHovering, setIsHovering] = useState(false);
+
+  console.log('isHovering', isHovering);
+  const PortfolioItem = ({ handleMouseOver, handleMouseOut }) => {
+    return (
+      <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        Hover Me
+      </div>
+    );
+  };
+
+  const PortfolioItemHovered = () => {
+    return (
+      <div>
+        Hovering right meow!
+        <span role="img" aria-label="cat">
+          🐱
+        </span>
+      </div>
+    );
+  };
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
     <>
-      <h1>Portfolio</h1>
+      <div>
+        {' '}
+        <PortfolioItem handleMouseOver={handleMouseOver} handleMouseOut={handleMouseOut} />
+        {isHovering && <PortfolioItemHovered />}
+      </div>
     </>
   );
 }
+
+export default Portfolio;
