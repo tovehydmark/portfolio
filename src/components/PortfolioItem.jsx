c716f5d6c2035464a7dd15ab3457659f225de474
tovehydmark <tovehydmark@gmail.com>
2023-04-16 19:59:43 +0200

    styling and icons

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,4 +1,5 @@
 import { React, useState } from 'react';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 export function PortfolioItem(props) {
   const [readMore, setReadMore] = useState(false);
@@ -7,12 +8,20 @@ export function PortfolioItem(props) {
     <div className="portfolio-item">
       <button onClick={() => setReadMore(!readMore)}>{readMore ? 'View less' : 'Read more'}</button>
       {readMore ? <div className="about-the-project">{props.about}</div> : null}
-      <a href={props.link} target="_blank" rel="noreferrer">
-        Visit the site at {props.name}
-      </a>
+      <p>
+        <a href={props.link} target="_blank" rel="noreferrer">
+          {' '}
+          Visit the published site
+          <FaExternalLinkAlt></FaExternalLinkAlt>
+        </a>
+      </p>
+
       {props.githubLink ? (
         <p>
-          or go to <a href={props.githubLink}>my github profile</a> to view the code
+          or view the code at{' '}
+          <a href={props.githubLink}>
+            github<FaExternalLinkAlt></FaExternalLinkAlt>
+          </a>
         </p>
       ) : null}
     </div>
